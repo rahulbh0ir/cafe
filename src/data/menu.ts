c80d3e9e33471d3b31d8aaf6656ac29f773e254b
Rahul Bhoir <rahulbhoir767676@gmail.com>
2026-01-07 11:32:12 +0530

    up

--- src/data/menu.ts
+++ src/data/menu.ts
@@ -1,11 +1,41 @@
-import { StaticImageData } from "next/image";
+// import { StaticImageData } from "next/image";
 
-export interface Property  {
-  id : string,
-  name : string,
-  img: StaticImageData,
-  description : string,
-  ingredients : string,
-  price : string,
-  categories : string,
-}
+
+
+// export interface Menu {
+//   id: string,
+//   name: string,
+//   img: StaticImageData,
+//   description: string,
+//   ingredients: string,
+//   price: string,
+//   categories: string,
+// }
+
+// const menu: Menu[] = [
+//   {
+//     id: "1",
+//     name: "Signature Cappuccino",
+//     img: {},
+//     description: "Bean There Signature Cappuccino",
+//     ingredients: "Coffee",
+//     price: "200",
+//     categories: "drinks",
+//   },
+//   {
+//     id: "2",
+//     name: "Signature Cappuccino",
+//     img: {},
+//     description: "Bean There Signature Cappuccino",
+//     ingredients: "Coffee",
+//     price: "200",
+//     categories: "drinks",
+//   },
+
+
+
+
+
+
+
+// ]
