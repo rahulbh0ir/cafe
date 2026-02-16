c3e73ba91987826b0000f81d686a82fa8c39eea2
Rahul Bhoir <rahulbhoir767676@gmail.com>
2026-02-16 11:15:21 +0530

    up

--- src/app/(public)/menu/page.tsx
+++ src/app/(public)/menu/page.tsx
@@ -5,6 +5,7 @@ import MainImg from "../../../../public/menu/menuhero.jpg";
 import menuDrinks from "../../../../public/menu/menu-1.jpg";
 import menuFood from "../../../../public/menu/menu-2.jpg";
 import menuPizza from "../../../../public/menu/menu-3.jpg";
+import { menuData } from "@/data/menu";
 
 export default function MenuPage() {
   return (
@@ -38,29 +39,14 @@ export default function MenuPage() {
         </div>
         <div className="menu-content">
           <h2>Coffee & <span>Drinks</span></h2>
-          <ul>
-            <li>
-              <span>Signature Cappuccino</span>
-              <span className="price">₹180</span>
-            </li>
-            <li>
-              <span>Classic Americano</span>
-              <span className="price">₹150</span>
-            </li>
-            <li>
-              <span>Cold Brew Coffee</span>
-              <span className="price">₹200</span>
-            </li>
-            <li>
-              <span>Chocolate Mocha</span>
-              <span className="price">₹220</span>
-            </li>
-            <li>
-              <span>Fresh Iced Lemonade</span>
-              <span className="price">₹140</span>
-            </li>
-          </ul>
-
+          {menuData.drinks.map(item => (
+            <ul key={item.id}>
+              <li>
+                <span>{item.name}</span>
+                <span className="price">₹ {item.price}</span>
+              </li>
+            </ul>
+          ))}
         </div>
       </section>
 
@@ -75,28 +61,15 @@ export default function MenuPage() {
         </div>
         <div className="menu-content">
           <h2>Fresh Food & <span>Bakes</span></h2>
-          <ul>
-            <li>
-              <span>Butter Croissants</span>
-              <span className="price">₹120</span>
-            </li>
-            <li>
-              <span>Garlic Bread</span>
-              <span className="price">₹100</span>
-            </li>
-            <li>
-              <span>Cheese Sandwich</span>
-              <span className="price">₹150</span>
-            </li>
-            <li>
-              <span>Chocolate Muffins</span>
-              <span className="price">₹80</span>
-            </li>
-            <li>
-              <span>Freshly Baked Cookies</span>
-              <span className="price">₹60</span>
-            </li>
-          </ul>
+          
+          {menuData.food.map(item => (
+            <ul key={item.id}>
+              <li>
+                <span>{item.name}</span>
+                <span className="price">₹ {item.price}</span>
+              </li>
+            </ul>
+          ))}
 
 
         </div>
@@ -113,29 +86,14 @@ export default function MenuPage() {
         </div>
         <div className="menu-content">
           <h2>Pizza & <span>Specials</span></h2>
-          <ul>
-            <li>
-              <span>Classic Margherita</span>
-              <span className="price">₹200</span>
-            </li>
-            <li>
-              <span>Farm Fresh Veggie Pizza</span>
-              <span className="price">₹180</span>
-            </li>
-            <li>
-              <span>Cheese Burst Pizza</span>
-              <span className="price">₹250</span>
-            </li>
-            <li>
-              <span>Herb Garlic Flatbread</span>
-              <span className="price">₹130</span>
-            </li>
-            <li>
-              <span>Chef’s Daily Special</span>
-              <span className="price">₹260</span>
-            </li>
-          </ul>
-
+          {menuData.pizza.map(item => (
+            <ul key={item.id}>
+              <li>
+                <span>{item.name}</span>
+                <span className="price">₹ {item.price}</span>
+              </li>
+            </ul>
+          ))}
 
         </div>
       </section>
