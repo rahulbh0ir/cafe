9a57e56bca99de7d9deeacdb0f7d527414736757
rahul bhoir <rahulbhoir767676@gmail.com>
2026-05-12 15:52:58 +0530

    up

--- src/app/(public)/test/page.tsx
+++ src/app/(public)/test/page.tsx
@@ -260,7 +260,7 @@ export default function HomePage() {
           <span className="map-label">LOCATION</span>
           <h2>Visit <span>Us</span></h2>
         </div>
-        <div className="map">
+        <div className="map" id="map">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2675.156154462806!2d73.81423652951185!3d18.517957927213054!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2bfb9e53a05f9%3A0x2be5e8da02be693e!2sMIT%20World%20Peace%20University%20(MIT-WPU)!5e0!3m2!1sen!2sin!4v1777889819500!5m2!1sen!2sin"
             width="600"
