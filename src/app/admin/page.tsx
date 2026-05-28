3ce1af60c89b1a660c96aecce5475bfb1d5de4ea
rahul bhoir <rahulbhoir767676@gmail.com>
2026-05-28 12:15:47 +0530

    up

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,23 +1,50 @@
 import TableCollection from "./table";
 import "./admin.css";
-
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "../../../public/header/logo4.png";
+import Spoon from "../../../public/admin/spoonW.png"
 
 export default async () => {
   return (
-    <div className="admin-page">
+    <main className="admin-page">
 
       <section className="admin-feature">
-        <div className="brand"></div>
-        <div className="content"></div>
+        <div className="admin-brand">
+          <Link href="/">
+            <Image
+              alt="logo"
+              src={Logo}
+              width={120}
+              height={70}
+              className="logo-img"
+            />
+          </Link>
+        </div>
+        <div className="admin-controls">
+          <div className="control-item">
+            <Image
+              src={Spoon}
+              alt="Table Reservation"
+              width={30}
+              height={30}
+            />
+            <p>Reservations</p>
+          </div>
+        </div>
       </section>
 
       <section className="admin-content">
-        <div className="admin-navbar"></div>
-        <div className="container">
+        <div className="admin-navbar">
+
+          <h3>Tabel Reservations</h3>
+          <input type="text" placeholder="Search..." />
+        </div>
+        <div className="admin-data">
           <TableCollection />
         </div>
       </section>
 
-    </div>
+    </main>
   );
 }
